2117a9db6e2be834309577385bbdac163c10abfb
skyota <kyotas3007@gmail.com>
2025-05-02 13:10:14 +0900

    mvの実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 'use client'
 
+import Image from 'next/image';
+import Button from './_components/Button';
+
 const Top: React.FC = () => {
   return (
-    <>
-      <section>
-        
+    <div className='bg-[#EBF6F6]'>
+      <section className='inner pt-24 pb-52 bg-contain bg-center bg-no-repeat bg-mv-pc md:bg-mv-sp md:pt-10 md:pb-14'>
+        <div className='flex items-center justify-between flex-row-reverse md:block'>
+          <div className='w-[42%] md:w-[70%] md:max-w-80 md:mx-auto'>
+            <Image src="/images/mv-screen.png" alt="操作画面" width={500} height={320} />
+          </div>
+          <div className='flex flex-col justify-center gap-10 md:mt-5 md:items-center gap-7'>
+            <h2 className='text-4xl text-fontcolor font-bold leading-relaxed md:text-2xl md:text-center'>家計を正しく整えるため<br />あなたに最適な予算を<br />自動で提案します。</h2>
+            <Button as='link' href='/signup' variant='blue'>新規登録はこちら</Button>
+          </div>
+        </div>
       </section>
-    </>
+    </div>
   )
 }
 
